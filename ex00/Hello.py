827f09ccfa7b69f3d462f505200094e1f24b746a
João <jocolognesi@gmail.com>
2026-09-06 22:18:24 -0300

    ex00

---
+++ ex00/Hello.py
@@ -0,0 +1,11 @@
+ft_list = ["Hello"]
+ft_tuple = ("Hello", "Brasil")
+ft_set = {"Hello", "Hello", "São Paulo"}
+ft_dict = {"Hello": "42SP"}
+
+ft_list.append("World")
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
